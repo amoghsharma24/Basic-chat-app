e833c72f88fc6988209afcc05e883f787b1d6821
amoghsharma24 <amoghsharma02@gmail.com>
2025-01-11 21:49:25 +0530

    main

--- index.js
+++ index.js
@@ -5,24 +5,32 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server); // will handle the sockets
+const io = new Server(server);
 
-//socket.io
-
+// Socket.IO Logic
 io.on("connection", (socket) => {
-  socket.on("user-message", (message) => {
-    io.emit("message", message);
+  console.log("A user connected");
+
+  socket.on("user-message", (data) => {
+    const { user, message } = data;
+    io.emit("message", { user, message }); // Broadcast message with user name
+    console.log(`${user}: ${message}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
   });
 });
 
-// trial commit
-
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Serve index.html
 app.get("/", (req, res) => {
-  return res.sendFile("/public/index.html");
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Start the server
 server.listen(3000, () => {
   console.log("Server is running on port: 3000");
 });
